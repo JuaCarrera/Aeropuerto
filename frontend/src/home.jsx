5b08fe4eb75f16e60ccd7e22e44d7960b70799a5
Valeria Guerrero <valeriaso.guerrero@umariana.edu.co>
2025-09-02 09:25:32 -0500

    feat: cambios en frontend inicial

---
+++ frontend/src/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenido al Aeropuerto</h1>
+      <p>Esta es la página principal.</p>
+    </div>
+  );
+}
+
+export default Home;
